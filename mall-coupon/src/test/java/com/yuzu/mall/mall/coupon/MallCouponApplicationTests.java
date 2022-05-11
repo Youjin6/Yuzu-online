e97e00fdd5579fcab9fa2dc9e090e80a8b667e37
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-05-10 22:28:51 -0700

    feat: Added initial worksapce

---
+++ mall-coupon/src/test/java/com/yuzu/mall/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzu.mall.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
